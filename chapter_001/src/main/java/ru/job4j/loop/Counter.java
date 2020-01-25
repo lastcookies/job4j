075e596313c2d8322cbb3a65862b007882f61e3a
lastcookies <9ruk94@bk.ru>
2020-01-25 17:59:00 +0500

    Подсчет суммы чётных чисел в диапазоне[#222579]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int index = 0; index <=10; index++) {
+            sum = sum + index;
+        }
+        return sum;
+    }
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int index = 0; index <= 10; index++) {
+            if (index % 2 == 0) {
+                sum = sum + index;
+            }
+        }
+        return sum;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOnToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat (rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEver() {
+        int rsl = Counter.sum(1, 10);
+        int expected = 55;
+        assertThat (rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbers() {
+        int rsl = Counter.sumByEven(1, 75);
+        int expected = 30;
+        assertThat (rsl, is(expected));
+    }
+
+}
