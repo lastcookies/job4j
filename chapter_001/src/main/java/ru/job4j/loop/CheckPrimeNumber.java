4f5eab5ec37d07b3cf96c588fc72171f0e21c9e0
lastcookies <9ruk94@bk.ru>
2020-01-28 21:56:10 +0500

    Урок по применению оператора break.

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int finish) {
-        boolean prime = false;
+        boolean prime = true;
         for (int index = 2; index <= finish; index++) {
-            if(index == finish) {
-                prime = true;
-                break;
-            }
-            if((finish % index) == 0) {
-                break;
+            if(finish % index == 0) {
+                if(finish != index) {
+                    prime = false;
+                    break;
+                }
             }
         } return prime;
     }
