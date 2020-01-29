48c0238f880f9e6995cc95341ed9b1370d2217ad
lastcookies <9ruk94@bk.ru>
2020-01-29 20:33:55 +0500

    Простое число.[#222577]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,12 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int finish) {
         boolean prime = true;
-        for (int index = 2; index <= finish; index++) {
+        for (int index = 2; index < finish; index++) {
             if(finish % index == 0) {
-                if(finish != index) {
                     prime = false;
                     break;
-                }
             }
         } return prime;
     }
