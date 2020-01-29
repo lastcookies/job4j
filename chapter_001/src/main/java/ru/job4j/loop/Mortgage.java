0f9821309e52c6f83dd56b21c679561aa22e5eb2
lastcookies <9ruk94@bk.ru>
2020-01-29 22:08:25 +0500

    Ипотека.[#222576]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year (int amount, int salary, double percent) {
+        // amount - сумма выданная по кредиту
+        // salary - годовой доход
+        // percent - процентная ставка по кредиту
+        int year = 0;
+        double x = amount;
+        while (x > 0) {
+            x = x * (percent / 100) + x - salary;
+            year++;
+        }
+        return year;
+    }
+}
