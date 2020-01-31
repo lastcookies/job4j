f29fb88a028cfe4c39c60e2a93a0d52f7a3de23c
lastcookies <9ruk94@bk.ru>
2020-01-31 21:07:40 +0500

    JavaDoc. Code convention[#222582]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,57 @@
 package ru.job4j.calculator;
 
+/**
+ * Package for calculate task.
+ *
+ * @author Yaroslav Starostin (9ruk94@bk.ru)
+ * @version [#222468]
+ * @since 0.1
+ */
+
 public class Calculator {
+
+    /**
+     * Addition
+     * @param first
+     * @param second
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + "+" + second + "=" + result);
     }
+    /**
+     * Division
+     * @param first
+     * @param second
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + "/" + second + "=" + result);
     }
+    /**
+     * Multiplication
+     * @param first
+     * @param second
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + "*" + second + "=" + result);
     }
+
+    /**
+     * Subtraction
+     * @param first
+     * @param second
+     */
     public static void subtract(double first, double second){
         double result = first - second;
         System.out.println(first + "-" + second + "=" + result);
     }
+
+    /**
+     * main
+     * @param args - args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/packaje-info.java
@@ -0,0 +1,7 @@
+/**
+* Calculator
+*
+* @author Yaroslav Starostin (9ruk94@bk.ru)
+* @version [#222468]
+* @since 0.1
+*/
