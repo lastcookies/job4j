763c8db8a22352e9508ff7a58d008f0dfe5ee0e9
lastcookies <9ruk94@bk.ru>
2020-02-01 11:50:11 +0500

    Крест в псевдографике. [#222578]

--- chapter_001/src/main/java/ru/job4j/loop/Slash.java
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -17,8 +17,8 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row == cell;
-                boolean right = size - 1 == row + cell;
+                boolean left = size - 1 == row + cell;
+                boolean right = row == cell;
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
