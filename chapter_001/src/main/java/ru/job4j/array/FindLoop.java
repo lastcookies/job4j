2f71140e8ccbdc6dcf391677d525a13c3039d1c6
lastcookies <9ruk94@bk.ru>
2020-02-06 21:25:00 +0500

    Поиск индекса в диапазоне.[#222522]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,12 +1,44 @@
 package ru.job4j.array;
 
+/**
+ * Поиск индекса в диапазоне.
+ *
+ * @author Yaroslav Starostin
+ * @version 2.0
+ * @since 6.02.2020
+ */
+
 public class FindLoop {
+    /**
+     * indexOf - проверяет, есть ли число в массиве
+     * @param data
+     * @param el
+     * @return
+     */
     public int indexOf(int[] data, int el) {
         int rst = -1;
         for (int index = 0; index < el; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * indexOf - проверяет, есть ли число в диапазоне чисел внутри массива
+     * @param data
+     * @param el
+     * @param start
+     * @param finish
+     * @return
+     */
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] <= finish && data[index] >= start && data[index] == el) {
+                rst = index;
             }
         }
         return rst;
