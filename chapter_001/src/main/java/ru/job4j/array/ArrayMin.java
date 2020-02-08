6f10082079e22a5d59a55f65cfffd76f7afeeba3
lastcookies <9ruk94@bk.ru>
2020-02-08 23:26:57 +0500

    Поиск минимального числа в массиве.[#222524]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayMin.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ *  Поиск минимального числа в массиве.
+ *
+ * @author Yaroslav Starostin
+ * @version 1.0
+ * @since 8.02.2020
+ */
+
+public class ArrayMin {
+
+    /**
+     * findMin - находит минимальное число в массиве
+     * @param array
+     * @return
+     */
+    public static int findMin(int [] array) {
+        int min = array[0];
+        for (int index = 0; index < array.length; index++) {
+            if (min > array[index]) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
+}
