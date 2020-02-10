fc654ab365248390dd63aa429da5c762ca4563e7
lastcookies <9ruk94@bk.ru>
2020-02-10 21:33:16 +0500

    Сортировка выборкой.[#222523]

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -1,7 +1,19 @@
 package ru.job4j.array;
 
-public class SortSelected {
+/**
+ * Сортировка выборкой.
+ *
+ * @author Starostin Yaroslav
+ * @version 1.0
+ * @since 10.02.2020
+ */
 
+public class SortSelected {
+    /**
+     * sort - выполняет сортировку чисел в массиве
+     * @param data - сортируемый массив чисел
+     * @return - отсортированный по возрастанию массив
+     */
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length);
