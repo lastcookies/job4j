5f41ac4a048ae4f919c83058dff93e2649a0453c
lastcookies <9ruk94@bk.ru>
2020-02-14 12:28:12 +0500

    Дефрагментация массива.[#222526]

--- chapter_001/src/main/java/ru/job4j/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -4,7 +4,7 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int point = index + 1;
+                int point = index;
                 while (array[index] == null && point < array.length) {
                     String temp = array[point];
                     array[point] = array[index];
