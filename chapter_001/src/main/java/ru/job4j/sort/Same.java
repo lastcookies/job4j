c67c358fb9a0d62c42b5f59327d96224317e16c4
lastcookies <9ruk94@bk.ru>
2020-02-18 12:20:16 +0500

    Поиск одинаковых чисел в массиве

---
+++ chapter_001/src/main/java/ru/job4j/sort/Same.java
@@ -0,0 +1,15 @@
+package ru.job4j.sort;
+
+public class Same {
+    public static void main(String[] args) {
+        int[] first = {1, 2, 3, 5, 7};
+        int[] second = {2, 3, 4, 7};
+        for (int i = 0; i < first.length; i++) {
+            for (int j = 0; j < second.length; j++) {
+                if (first[i] == second[j]) {
+                    System.out.print(first[i] + " ");
+                }
+            }
+        }
+    }
+}
--- chapter_001/src/test/java/ru/job4j/array/endsWithTest.java
+++ chapter_001/src/test/java/ru/job4j/array/endsWithTest.java
@@ -19,7 +19,7 @@ public class EndsWithTest {
     public void whenEndWithPrefixThenFalse() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
         char[] post = {'l', 'a'};
-        boolean result = endsWith.endsWith(word, post);
+        boolean result = EndsWith.endsWith(word, post);
         assertThat(result, is(false));
     }
 
