55b516b652635d441aa9de119af6072ad8c74fc0
lastcookies <9ruk94@bk.ru>
2020-03-24 13:54:02 +0500

    2.2. Рефакторинг - Площадь треугольника.[#222667]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,10 +1,61 @@
 package ru.job4j.condition;
 
-public class Triangle {
+import static java.lang.Math.sqrt;
 
-    public static boolean exist(double ab, double bc, double ca) {
-        boolean condition = ((ab + bc) > ca) && ((ab + ca) > bc) && ((bc + ca) > ab);
-        boolean result = condition ? true : false;
-        return result;
+/**
+ * Площадь треугольника
+ *
+ * @author Yaroslav Starostin (9ruk94@bk.ru)
+ * @version 1.0
+ * @since 24.03.2020
+ */
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     *Метод должен вычислить периметр треугольника
+     * @param a расстояние между точками a и b
+     * @param b расстояние между точками a и c
+     * @param c расстояние между точками b и с
+     * @return p - периметр треугольника
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    /**
+     * метод должен вычислить площадь трекгольника
+     * @return s - площадь трегольника
+     */
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b , c)) {
+            rsl = Math.sqrt(p *(p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет, может ли существовать треугольник с данными длинами сторон
+     * @param a расстояние между точками a и b
+     * @param b расстояние между точками a и c
+     * @param c расстояние между точками c и b
+     * @return результат проверки
+     */
+    private boolean exist(double a, double b, double c) {
+        return (a + b) > c && (a + c) > b && (c + b) > a;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -8,14 +8,24 @@ public class TriangleTest {
 
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+        Point first = new Point(2, 2);
+        Point second = new Point(2, 4);
+        Point third = new Point(5, 6);
+        Triangle tri = new Triangle(first, second, third);
+        double result = tri.area();
+        assertThat(result, is(3.000000000000001));
     }
 
     @Test
     public void whenNoExist() {
-        boolean result = Triangle.exist(2.0, 3.0, 7.0);
-        assertThat(result, is(false));
+        Point first = new Point(2, 2);
+        Point second = new Point(2, 2);
+        Point third = new Point(3, 3);
+        Triangle tri = new Triangle(first, second, third);
+        double result = tri.area();
+        assertThat(result, is(-1.0));
     }
 }
+
+
 
