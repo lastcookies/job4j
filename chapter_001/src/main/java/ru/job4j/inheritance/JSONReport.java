6a3bf4ef32dedcc64f2ef658fea5cc950b51e2d7
lastcookies <9ruk94@bk.ru>
2020-04-10 22:12:42 +0500

    4. Переопределение[#222683]

---
+++ chapter_001/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + "\n" +
+                name + "\n" +
+                body + "\n" +
+                "}";
+    }
+}
